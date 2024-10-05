d8214bc347209ca61cd43ccd13e2837296e1292c
Alex Fu <a22fu@uwaterloo.ca>
2024-10-04 21:24:27 -0400

    frontend prettier

--- vct-chatbot/src/components/PlayerCard.jsx
+++ vct-chatbot/src/components/PlayerCard.jsx
@@ -12,13 +12,13 @@ export function Item(props) {
   const { id } = props;
 
   const style = {
-    width: "100%",
-    height: 50,
+    width: "9vw",
+    height: "9vw",
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     border: "1px solid black",
-    margin: "10px 0",
+
     background: "white"
   };
 
---
+++ vct-chatbot/src/components/SearchWindow.jsx
@@ -0,0 +1,17 @@
+// components/ChatWindow.js
+import React, { useState } from 'react';
+import ChatMessage from './ChatMessage';
+import ChatInput from './ChatInput';
+// App.js
+import PlayerBox from './PlayerBox.jsx';
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  DragOverlay,
+
+  useSensor,
+  useSensors,
+  closestCorners,
+} from "@dnd-kit/core";
+import { arrayMove, sortableKeyboardCoordinates } from "@dnd-kit/sortable";
