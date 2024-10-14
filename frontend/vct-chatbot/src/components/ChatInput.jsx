6ed507e858bbfd061d774ddf1eb83a532d4d35d9
Colin Ruan <colin.ruan@gmail.com>
2024-10-14 00:08:35 -0400

    some css changes

--- frontend/vct-chatbot/src/components/ChatInput.jsx
+++ frontend/vct-chatbot/src/components/ChatInput.jsx
@@ -18,34 +18,21 @@ const ChatInput = ({ onSendMessage }) => {
   return (
     <div className="chat-input">
       <div className="input-container">
-      <div class="valorant-icon">
-  <img src="../resources/vallogo.png" ></img>
-</div>
-  <input
-    type="text"
-    value={inputValue}
-    onChange={(e) => setInputValue(e.target.value)}
-    onKeyDown={(e) => handleEnter(e)}
-    placeholder="Ask a VCT question..."
-    className="message-input"
-  />
-  <button onClick={handleSend} className="send-button">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="feather feather-upload"
-    >
-      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
-      <polyline points="17 8 12 3 7 8"></polyline>
-      <line x1="12" y1="3" x2="12" y2="15"></line>
-    </svg>
-  </button>
-</div>
+        <div className="valorant-icon">
+          <img src="../resources/vallogo.png" ></img>
+        </div>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+          onKeyDown={(e) => handleEnter(e)}
+          placeholder="Ask a VCT question..."
+          className="message-input"
+        />
+        <button onClick={handleSend} className="send-button">
+          <img width="37px" height="37px" src="../resources/send-svgrepo-com.svg"/>
+        </button>
+        </div>
 
     </div>
   );
