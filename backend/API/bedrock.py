4aecc4c6c52403abed4cbb6a0c6f92dd5bef2ed1
Alex Fu <a22fu@uwaterloo.ca>
2024-10-18 23:23:03 -0400

    oops

--- backend/API/bedrock.py
+++ backend/API/bedrock.py
@@ -11,9 +11,8 @@ from prompt_templates import *
 
 
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS')
-print(aws_access_key_id)
-print(aws_secret_access_key)
+aws_secret_access_key = os.getenv('AWS_SECRET_KEY')
+
 class VctClient():
     
     def __init__(self,
