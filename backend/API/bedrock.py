65753c6d8e9bc8871fa09823d60c1f16441b143f
Alex Fu <a22fu@uwaterloo.ca>
2024-10-23 18:00:07 -0400

    add credentials

--- backend/API/bedrock.py
+++ backend/API/bedrock.py
@@ -10,8 +10,8 @@ import awswrangler as wr
 from prompt_templates import *
 
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY')
-aws_secret_access_key = os.getenv('AWS_SECRET_KEY')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 class VctClient():
     
